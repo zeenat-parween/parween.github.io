d2029f3681eb720304bfe8ed26bc2417786ca471
zeenat <pzeenat740@gmail.com>
2021-08-02 04:42:42 +0530

    wow

---
+++ about.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@1&display=swap" rel="stylesheet">
+
+ </head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+
+<div class="jumbotron">
+  <h1>zeenat parween :)</h1>
+  <p></p>
+</div>
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+        <img src="images/p4.jpg" class="img-fluid aboutimg">
+      </div>
+
+            <div class="col-lg-6 col-md-6 col-12">
+            <h2>I am zeenat parween.</h2>
+            <p>“Love is the flower you’ve got to let grow.” – John Lennon<br>
+“A flower cannot blossom without sunshine, and man cannot live without love.” – Max<br>  
+“Life is the flower for which love is the honey.” – Victor Hugo<br>
+“If I had a single flower for every time I think about you, I could walk forever in my garden.” – Claudia Adrienne Grandi<br>
+“Love is like a beautiful flower which I may not touch, but whose fragrance makes the garden a place of delight just the same.” – Helen Keller<br>
+“If we could see the miracle of a single flower clearly our whole life would change.” – Buddha<br>
+“Minds are like flowers; they open only when the time is right.” – Stephen Richards<br>
+“Flowers… are a proud assertion that a ray of beauty out values all the utilities in the world.” – Ralph Waldo Emerson<br>
+“A rose can never be a sunflower, and a sunflower can never be a rose. All flowers are beautiful in their own way, and that’s like women too.” – Miranda Kerr<br>
+“Love is flower like; Friendship is like a sheltering tree.” – Samuel Taylor Coleridge
+</p>
+<hr><hr><hr>
+<br><br>
+“The rose is the flower and handmaiden of love – the lily, her fair associate, is the emblem of beauty and purity.” – Dorothea Dix<br>
+“Flowers… are a proud assertion that a ray of beauty out values all the utilities in the world.” – Ralph Waldo Emerson<br>
+“Every flower is a soul blossoming in nature.” – Gerard De Nerval<br>
+“Happiness held is the seed; Happiness shared is the flower.” – John Harrigan<br>
+“Politeness is the flower of humanity.” – Joseph Joubert<br>
+“Flowers are like friends; They bring color to your world.” – Unknown<br>
+“Earth laughs in flowers.” – Ralph Waldo Emerson<br>
+“Flowers don’t tell, they show.” – Stephanie Skeem<br>
+“Flowers are the music of the ground. From earth’s lips spoken without sound.” – Edwin Curran<br>
+“Flowers always make people better, happier and more helpful; they are sunshine, food and medicine for the soul.” – Luther Burbank<br>
+
+            <a href="about.php" class="btn btn-success"> Check More </a>
+
+</div>
+  </div>
+
+</section>
+
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">Life is like a box of chocolates, and you might be searching through the wrappers wondering if you should order another case. If you plan on being crowned queen of summer, arm yourself with these inspirational affirmations as you prepare for the new season. Scholars, songbirds and stars of the silver screen alike share their secrets to living well.</p>
+</footer>
+
+
+</body>
+</html>
